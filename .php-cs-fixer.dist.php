b04b5f1646baea5a2e214ca14e38fd393675b5c6
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-10-09 21:52:27 +0200

    prepare release

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,50 +10,54 @@
  */
 
 $finder = PhpCsFixer\Finder::create()
-  ->name('*.php')
-  ->ignoreDotFiles(TRUE)
-  ->ignoreVCS(TRUE);
+    ->in(__DIR__ . '/src')
+    ->append([__FILE__])
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
 
 $config = new PhpCsFixer\Config();
 $config->setFinder($finder)
-  ->setRules([
-    '@PSR12' => TRUE,
-    'array_syntax' => ['syntax' => 'short'],
-    'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'no_unused_imports' => TRUE,
-    'not_operator_with_successor_space' => TRUE,
-    'trailing_comma_in_multiline' => TRUE,
-    'phpdoc_scalar' => TRUE,
-    'unary_operator_spaces' => TRUE,
-    'binary_operator_spaces' => TRUE,
-    'blank_line_before_statement' => [
-      'statements' => [
-        'break',
-        'continue',
-        'declare',
-        'return',
-        'throw',
-        'try',
-      ],
-    ],
-    'phpdoc_single_line_var_spacing' => TRUE,
-    'phpdoc_var_without_name' => TRUE,
-    'class_attributes_separation' => [
-      'elements' => [
-        'method' => 'one',
-      ],
-    ],
-    'braces' => [
-      'allow_single_line_closure' => TRUE,
-      'position_after_anonymous_constructs' => 'same',
-      'position_after_functions_and_oop_constructs' => 'next',
-      'position_after_control_structures' => 'same',
-    ],
-    'method_argument_space' => [
-      'on_multiline' => 'ensure_fully_multiline',
-      'keep_multiple_spaces_after_comma' => TRUE,
-    ],
-    'single_trait_insert_per_statement' => TRUE,
-  ]);
+    ->setRules(
+        [
+            '@PSR12' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'ordered_imports' => ['sort_algorithm' => 'alpha'],
+            'no_unused_imports' => true,
+            'not_operator_with_successor_space' => true,
+            'trailing_comma_in_multiline' => true,
+            'phpdoc_scalar' => true,
+            'unary_operator_spaces' => true,
+            'binary_operator_spaces' => true,
+            'blank_line_before_statement' => [
+              'statements' => [
+                'break',
+                'continue',
+                'declare',
+                'return',
+                'throw',
+                'try',
+              ],
+            ],
+            'phpdoc_single_line_var_spacing' => true,
+            'phpdoc_var_without_name' => true,
+            'class_attributes_separation' => [
+              'elements' => [
+                'method' => 'one',
+              ],
+            ],
+            'braces' => [
+              'allow_single_line_closure' => true,
+              'position_after_anonymous_constructs' => 'same',
+              'position_after_functions_and_oop_constructs' => 'next',
+              'position_after_control_structures' => 'same',
+            ],
+            'method_argument_space' => [
+              'on_multiline' => 'ensure_fully_multiline',
+              'keep_multiple_spaces_after_comma' => true,
+            ],
+            'single_trait_insert_per_statement' => true,
+        ]
+    );
 
 return $config;
