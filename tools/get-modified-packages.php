4c527e4df1af9773eb403f3116501ff6f78a47e1
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-10-21 06:41:12 +0200

    Optimize workflows for split repositories

---
+++ tools/get-modified-packages.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the aether/aether.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+$packages = \explode(PHP_EOL, \trim(\shell_exec("git diff HEAD --name-only packages/")));
+$modifiedPackages = [];
+
+foreach($packages as $package) {
+    $directory = \explode(DIRECTORY_SEPARATOR, $package)[1];
+    $composerFile = \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'packages' . DIRECTORY_SEPARATOR . $directory . DIRECTORY_SEPARATOR . 'composer.json';
+    $composerData = \json_decode(\file_get_contents($composerFile));
+
+    if (!$composerData->name) {
+        continue;
+    }
+
+    $modifiedPackages[$composerData->name] = [
+        'name' => $composerData->name,
+        'repository_name' => \str_replace('/', '-', $composerData->name),
+        'package_directory' => $directory,
+    ];
+}
+
+$output = [];
+$output['include'] = $modifiedPackages;
+
+var_export($output);
