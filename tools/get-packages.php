be30fa9e35f0d90756e571c897a7a138653387c5
Dominik Szamburski <dominikszamburski99@gmail.com>
2025-01-01 09:21:59 +0100

    Update copyright date (#11)

--- tools/get-packages.php
+++ tools/get-packages.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the aether/aether.
+ *
+ * Copyright (C) 2024-2025 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 $packages = [];
 
 foreach (\glob(\dirname(__DIR__) . '/packages/*/composer.json') as $package) {
